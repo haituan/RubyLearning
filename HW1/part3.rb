e6f237a499260e7ab7c6d98d64233f506bd8c966
Tuan Hai <tuanhai619@gmail.com>
2016-09-20 15:40:34 +0700

    Update part3.rb

--- HW1/part3.rb
+++ HW1/part3.rb
@@ -1,1 +1,20 @@
 
+class BookInStock
+  attr_accessor :isbn, :price
+  def initialize(is, pri)
+    if (!is.empty? && pri > 0.0)
+      @isbn = is
+      @price = pri
+    else
+      raise ArgumentError
+    end
+  end
+	
+  def price_as_string
+    str = @price.round(2).to_s
+    if (str[str.size-2] == '.')
+      str += '0'
+    end
+    puts '$' + str
+  end
+end
