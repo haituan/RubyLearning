77c1042bcba77106c42e5bf20b34a9fda2c26039
Tuan Hai <tuanhai619@gmail.com>
2016-09-26 17:14:21 +0700

    Add files via upload

---
+++ HW3/app/controllers/movies_controller.rb
@@ -0,0 +1,60 @@
+class MoviesController < ApplicationController
+
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
+  end
+
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movie.find(id) # look up movie by unique ID
+    # will render app/views/movies/show.<extension> by default
+  end
+
+  def index 
+    @all_ratings = Movie.uniq.pluck(:rating)
+
+	if params[:ratings]
+	  @movies = Movie.where(:rating => params[:ratings].keys)
+	  session[:selected_rating] = params[:ratings]
+	elsif params[:sort_by]
+	  @column = params[:sort_by]
+	  @movies = Movie.order(params[:sort_by])
+	else
+	  if session[:selected_rating]
+	    @movies = Movie.where(:rating => session[:selected_rating].keys)
+	  else
+	    @movies = Movie.all
+	  end
+	end
+
+  end
+  
+  def new
+    # default: render 'new' template
+  end
+
+  def create
+    @movie = Movie.create!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
+
+  def edit
+    @movie = Movie.find params[:id]
+  end
+
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
+  end
+
+end
